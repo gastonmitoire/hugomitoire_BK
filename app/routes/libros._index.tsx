2d9f91b8486db92d8b0abd6e84201dcd27c6e032
Gaston Mitoire <gastonmm@gmail.com>
2023-05-09 10:54:08 -0300

    feat: book animations

--- app/routes/libros._index.tsx
+++ app/routes/libros._index.tsx
@@ -1,6 +1,7 @@
 import type { V2_MetaFunction, LoaderArgs } from "@remix-run/node";
 import { json } from "@remix-run/node";
 import { Form, Link, Outlet, useLoaderData } from "@remix-run/react";
+import { motion } from "framer-motion";
 
 import { Book } from ".prisma/client";
 import { db } from "~/utils/db.server";
@@ -20,16 +21,24 @@ export const loader = async ({ request }: LoaderArgs) => {
 export default function LibrosIndex() {
   const data = useLoaderData<typeof loader>();
   const bookListItems = data.bookListItems;
+
   return (
-    <div className="grid grid-cols-5 gap-3">
+    <motion.div
+      className="grid grid-cols-5 gap-3"
+      initial={{ opacity: 0, y: 50 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.5 }}
+    >
       {bookListItems.map(({ cover, title }, index) => (
-        <img
+        <motion.img
           key={index}
           src={cover}
           alt={title}
-          className="w-full h-full object-cover"
+          className="w-full h-full object-cover cursor-pointer"
+          whileHover={{ scale: 1.05 }} // animación al pasar el mouse sobre la imagen
+          whileTap={{ scale: 0.95 }} // animación al hacer clic sobre la imagen
         />
       ))}
-    </div>
+    </motion.div>
   );
 }
