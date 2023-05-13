27d8a97ab0d7f19f91e0939e5f62216c843115cf
Gaston Mitoire <gastonmm@gmail.com>
2023-05-13 08:32:13 -0300

    feat: book page design rework

---
+++ app/components/book.tsx
@@ -0,0 +1,60 @@
+import React from "react";
+import { motion } from "framer-motion";
+
+interface BookProps {
+  title: string;
+  description: string;
+  type: string;
+  genre: {
+    name: string;
+  };
+  cover: string;
+}
+
+export function Book({ title, description, type, genre, cover }: BookProps) {
+  return (
+    <div className="flex h-full rounded-xl dark:bg-neutral-900 dark:bg-opacity-70">
+      <motion.div
+        initial={{ opacity: 0, x: 10 }}
+        animate={{ opacity: 1, x: 0 }}
+        exit={{ opacity: 0, x: 10 }}
+        transition={{ duration: 0.9 }}
+        className="flex flex-col gap-y-3 py-3 pl-3 h-full"
+      >
+        <p className="text-xl font-bold text-white text-right font-bellefair py-1 px-3 rounded-l-xl dark:text-neutral-800 dark:bg-neutral-200 dark:bg-opacity-90">
+          {description}
+        </p>
+        <div className="pr-3 text-right font-cinzel dark:text-neutral-300">
+          <h5 className="italic uppercase">{type}</h5>
+          <strong className="lowercase pl-10">{genre.name}</strong>
+          <h1 className="pb-3 pt-5 pl-5 text-5xl font-bold text-white text-right">
+            {title}
+          </h1>
+        </div>
+        <div className="h-full flex justify-evenly items-end">
+          <span>
+            <h6 className="text-xs text-right font-bold italic text-neutral-500 uppercase">
+              Ilustrador
+            </h6>
+            <p className="text-lg font-sans">Maco Pacheco</p>
+          </span>
+          <span>
+            <h6 className="text-xs text-right font-bold italic text-neutral-500 uppercase">
+              Editorial
+            </h6>
+            <p className="text-lg font-sans">Ediciones de La Paz</p>
+          </span>
+        </div>
+      </motion.div>
+      <motion.img
+        initial={{ opacity: 0, scale: 0.9 }}
+        animate={{ opacity: 1, scale: 1 }}
+        exit={{ opacity: 0, scale: 0.9 }}
+        transition={{ duration: 0.5 }}
+        src={cover}
+        alt=""
+        className="h-full mx-auto object-contain shadow-md shadow-neutral-950"
+      />
+    </div>
+  );
+}
--- app/routes/libros.$bookTitle.tsx
+++ app/routes/libros.$bookTitle.tsx
@@ -10,9 +10,11 @@ import {
 } from "@remix-run/react";
 import { motion, AnimatePresence } from "framer-motion";
 
+import { Chapter } from "@prisma/client";
 import { db } from "~/utils/db.server";
 
-import { Chapter } from ".prisma/client";
+import { Tabs } from "~/components/tabs";
+import { Book } from "~/components/book";
 
 export const loader = async ({ params }: LoaderArgs) => {
   const formatedTitle = params.bookTitle?.replace(/_/g, " ");
@@ -28,87 +30,73 @@ export const loader = async ({ params }: LoaderArgs) => {
     );
   }
 
-  return json(book);
+  return json({ book });
 };
 
 export default function LibroRoute() {
-  const data = useLoaderData();
+  const { book } = useLoaderData();
+
+  console.log("book", book);
 
   return (
     <div
-      className="h-[90vh] w-full bg-cover bg-center py-3 px-1 md:px-0"
+      className="h-[90vh] w-full bg-cover bg-center"
       style={{
-        backgroundImage: `url(${data.secondaryImage})`,
+        backgroundImage: `url(${book.secondaryImage})`,
         backgroundAttachment: "fixed",
         backgroundRepeat: "no-repeat",
       }}
     >
-      <div className="container mx-auto p-3 grid grid-cols-1 md:grid-cols-3 md:grid-rows-2 gap-3 h-full w-full rounded-xl dark:bg-neutral-900 dark:bg-opacity-50">
-        <div className="h-full md:col-span-2 grid grid-cols-1 md:grid-cols-2 gap-3 md:gap-0">
-          <motion.img
-            initial={{ x: 150 }}
-            animate={{ x: 0 }}
-            exit={{ x: 150 }}
-            transition={{ duration: 1 }}
-            src={data.cover}
-            alt={data.title}
-            className="md:h-[80vh] w-full object-contain z-10"
-          />
-          <div className="flex flex-col gap-3 md:gap-0 h-full md:overflow-y-auto">
-            <motion.h3
-              initial={{ opacity: 0, y: -10 }}
-              animate={{ opacity: 1, y: 0 }}
-              exit={{ opacity: 0, y: -10 }}
-              transition={{ duration: 0.5 }}
-              className="text-xl text-center font-light font-cinzel lowercase py-3 dark:bg-neutral-900 dark:bg-opacity-50"
-            >
-              Indice
-            </motion.h3>
-            <AnimatePresence>
-              <motion.ul className="flex-1 flex flex-col gap-1 min-h-min">
-                {data.chapters.map(({ id, title, order }: Chapter) => (
-                  <motion.li
-                    initial={{ opacity: 0, x: -100 }}
-                    animate={{ opacity: 1, x: 0 }}
-                    exit={{ opacity: 0, x: -100 }}
-                    transition={{ duration: 0.7, delay: order * 0.1 }}
-                    key={id}
-                    className="py-3 px-3 cursor-pointer dark:bg-neutral-900 dark:bg-opacity-80 hover:dark:bg-opacity-95"
-                  >
-                    <Link
-                      to={`/libros/${data.title.replace(/ /g, "_")}/${id}`}
-                      className="font-cinzel text-lg dark:text-neutral-300"
-                    >
-                      {order} | {title}
-                    </Link>
-                  </motion.li>
-                ))}
-              </motion.ul>
-            </AnimatePresence>
-            <motion.div
-              initial={{ opacity: 0, y: 10 }}
-              animate={{ opacity: 1, y: 0 }}
-              exit={{ opacity: 0, y: 10 }}
-              transition={{
-                duration: 0.5,
-              }}
-              className="py-1.5 px-3 text-center dark:text-neutral-300 cursor-pointer dark:bg-neutral-900 dark:bg-opacity-50"
-            >
-              .
-            </motion.div>
+      <div className="h-full bg-neutral-500 bg-opacity-75">
+        <div className="container mx-auto grid grid-cols-3 grid-rows-3 h-full w-full p-3">
+          <div className="col-span-2 row-span-3">
+            <Book {...book} />
           </div>
-        </div>
-        <div className="flex flex-col md:items-end gap-1 h-full">
-          <p className="text-xl font-bold text-white text-right font-bellefair py-1 px-3 rounded-xl dark:text-neutral-800 dark:bg-neutral-200 dark:bg-opacity-90">
-            {data.description}
-          </p>
-          <span className="flex items-center gap-1 font-cinzel dark:text-neutral-300">
-            <h5 className="italic uppercase">{data.type}</h5>|
-            <strong className="lowercase">{data.genre.name}</strong>
-          </span>
-          <h1 className="text-3xl font-bold text-white text-right">
-            {data.title}
-          </h1>
+          <div className="row-span-3">
+            <div className="flex flex-col gap-3 md:gap-0 h-full md:overflow-y-auto">
+              <motion.h3
+                initial={{ opacity: 0, y: -10 }}
+                animate={{ opacity: 1, y: 0 }}
+                exit={{ opacity: 0, y: -10 }}
+                transition={{ duration: 0.5 }}
+                className="text-xl text-center font-light font-cinzel lowercase py-3 dark:bg-neutral-900 dark:bg-opacity-50"
+              >
+                Indice
+              </motion.h3>
+              <AnimatePresence>
+                <motion.ul className="flex-1 flex flex-col gap-1 min-h-min">
+                  {book.chapters.map(({ id, title, order }: Chapter) => (
+                    <motion.li
+                      initial={{ opacity: 0, x: -100 }}
+                      animate={{ opacity: 1, x: 0 }}
+                      exit={{ opacity: 0, x: -100 }}
+                      transition={{ duration: 0.7, delay: order * 0.1 }}
+                      key={id}
+                      className="py-3 px-3 cursor-pointer dark:bg-neutral-900 dark:bg-opacity-80 hover:dark:bg-opacity-95"
+                    >
+                      <Link
+                        to={`/libros/${book.title.replace(/ /g, "_")}/${id}`}
+                        className="font-cinzel text-lg dark:text-neutral-300"
+                      >
+                        {order} | {title}
+                      </Link>
+                    </motion.li>
+                  ))}
+                </motion.ul>
+              </AnimatePresence>
+              <motion.div
+                initial={{ opacity: 0, y: 10 }}
+                animate={{ opacity: 1, y: 0 }}
+                exit={{ opacity: 0, y: 10 }}
+                transition={{
+                  duration: 0.5,
+                }}
+                className="py-1.5 px-3 text-center dark:text-neutral-300 cursor-pointer dark:bg-neutral-900 dark:bg-opacity-50"
+              >
+                .
+              </motion.div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
