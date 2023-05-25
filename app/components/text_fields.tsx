dc50b30e1c6fe19e72f5a0eb0e82175bb71de7c4
Gaston Mitoire <gastonmm@gmail.com>
2023-05-25 11:23:56 -0300

    chore: markdown editor removed2

--- app/components/text_fields.tsx
+++ app/components/text_fields.tsx
@@ -1,5 +1,3 @@
-import { Editable, useEditor } from "@wysimark/react";
-
 import { Text } from "@prisma/client";
 import React from "react";
 
@@ -9,26 +7,9 @@ interface TextFieldsProps {
 }
 
 export function TextFields({ text, chapterId }: TextFieldsProps) {
-  const [editable, setEditable] = React.useState(false);
-  const [content, setContent] = React.useState(
-    "# Hello World\n\nLorem ipsum dolar."
-  );
-  const editor = useEditor({
-    initialMarkdown: text?.content ?? content,
-    height: 240,
-    authToken: "my-auth-token",
-  });
-
-  console.log("editor ", editor.getMarkdown());
-
   return (
     <div className="grid gap-5">
-      {editable ? (
-        <Editable editor={editor} />
-      ) : (
-        <div>{text?.content ?? content}</div>
-      )}
-      <input type="hidden" name="content" value={content} />
+      <input type="hidden" name="content" value={""} />
 
       <input type="hidden" name="chapterId" value={chapterId} />
     </div>
