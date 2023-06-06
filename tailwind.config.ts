9dd544acdc9e0691f286e29f9da18db917b21bca
Gaston Mitoire <gastonmm@gmail.com>
2023-06-05 23:52:12 -0300

    feat: tailwind container config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,20 +4,34 @@ export default {
   darkMode: "class",
   content: ['./app/**/*.{js,jsx,ts,tsx}'],
   theme: {
+    // default breakpoints but with 40px removed
+    screens: {
+      'sm': {'min': '640px', 'max': '767px'},
+      // => @media (min-width: 640px and max-width: 767px) { ... }
+
+      'md': {'min': '768px', 'max': '1023px'},
+      // => @media (min-width: 768px and max-width: 1023px) { ... }
+
+      'lg': {'min': '1024px', 'max': '1279px'},
+      // => @media (min-width: 1024px and max-width: 1279px) { ... }
+
+      'xl': {'min': '1280px', 'max': '1535px'},
+      // => @media (min-width: 1280px and max-width: 1535px) { ... }
+
+      '2xl': {'min': '1536px'},
+      // => @media (min-width: 1536px) { ... }
+    },
     container: {
       // you can configure the container to be centered
-      center: true,
+      // center: true,
 
       // or have default horizontal padding
-      padding: '1rem',
-
-      // default breakpoints but with 40px removed
-      screens: {
-        sm: '600px',
-        md: '728px',
-        lg: '984px',
-        xl: '1240px',
-        '2xl': '1496px',
+      padding: {
+        DEFAULT: '1rem',
+        sm: '2rem',
+        lg: '4rem',
+        xl: '5rem',
+        '2xl': '6rem',
       },
     },
     extend:{
